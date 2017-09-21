24ee4f9dfb39090e0df0a3c7f6d52b78b3ef45b5
Bence Tilk <bence.tilk@gmail.com>
2017-09-21 18:55:29 +0200

    space fix

--- src/main/java/org/json/simple/ItemList.java
+++ src/main/java/org/json/simple/ItemList.java
@@ -41,8 +41,7 @@ public class ItemList {
 	
 	public String[] getArray(){
 		String[] Arr=new String[this.item.size()];
-		Arr= this.item.toArray(Arr);
-		return Arr;
+	        return this.item.toArray(Arr);
 	}
 	
 	public void split(String s,String sp,List append,boolean isMultiToken){
